1c232eb11de03fc23b8a6af07a215de2dea6e97f
GregZapu <127511796+GregZapu@users.noreply.github.com>
2024-01-22 23:29:09 +0300

    Update for_testing.py
    Normal bullets

--- for_testing.py
+++ for_testing.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from copy import copy
 
 
@@ -9,19 +10,30 @@ class Bullet:
         self.end_pos = end_pos
         self.bullet_appear = True
         self.modifyers =  [1, 1]
+        self.lifetime = 60
         x_lengh = self.current_pos[0] - self.end_pos[0]
         y_lengh = self.current_pos[1] - self.end_pos[1]
-        self.y_x = y_lengh / x_lengh
-
-        
+        try:
+            self.yx = y_lengh / x_lengh
+            self.g = (y_lengh ** 2 + x_lengh ** 2) ** 0.5
+            if current_pos[1] > end_pos[1]:
+                self.he = -1
+            else:
+                self.he = 1
+            self.an = math.asin(x_lengh / self.g)
+        except:
+            self.bullet_appear = False
+            
         
     def render(self, screen):
-        for i in range(10 - max(self.modifyers)):
-            pygame.draw.circle(screen, ("green"), (self.current_pos[0], self.current_pos[1]), 5)
-            self.current_pos[0] += 1
-            self.current_pos[1] = int(self.y_x + self.current_pos[1])
-            if self.current_pos[1] == self.end_pos[1] and self.current_pos[0] == self.end_pos[0]:
-                self.bullet_appear = False
+        pygame.draw.circle(screen, ("green"), (int(self.current_pos[0]), int(self.current_pos[1])), 5)
+        self.current_pos[0] += 1 * math.sin(self.an) * 5 * -1
+        self.current_pos[1] += 1 * math.cos(self.an) * 5 * self.he
+        self.lifetime -= 1
+        if self.lifetime == 0:
+            self.bullet_appear = False
+        if self.current_pos[1] == self.end_pos[1] and self.current_pos[0] == self.end_pos[0]:
+            self.bullet_appear = False
 
 
 
@@ -32,22 +44,6 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     running = True
     bullet_storage = []
-
-    all_sprites = pygame.sprite.Group()
-    sprite = pygame.sprite.Sprite()
-
-    image = pygame.image.load("testing_pic.png")
-    image1 = pygame.transform.scale(image, (50, 40))
-    image3 = pygame.image.load("testing_pic2.png")
-    image4 = pygame.transform.scale(image3, (50, 40))
-
-    sprite.image = image1
-    all_sprites.add(sprite)
-    sprite.image = image4
-    all_sprites.add(sprite)
-    sprite.rect = sprite.image.get_rect()
-    sprite.rect.x = 100
-    sprite.rect.y = 100
 
     v = 20  # пикселей в секунду
     fps = 60
@@ -88,8 +84,6 @@ if __name__ == '__main__':
 
         character_coordinates[0] += movement_speed[0]
         character_coordinates[1] += movement_speed[1]
-        sprite.rect.x = character_coordinates[0] - 20
-        sprite.rect.y = character_coordinates[1] - 20
 
         if character_coordinates[0] > boss_coordinates[0]:
             boss_coordinates[0] += 2
@@ -99,12 +93,12 @@ if __name__ == '__main__':
             boss_coordinates[1] += 2
         elif character_coordinates[1] < boss_coordinates[1]:
             boss_coordinates[1] -= 2     
-        all_sprites.draw(screen)
         for elem in bullet_storage:
             if elem.bullet_appear == True:
                 elem.render(screen)
             else:
                 bullet_storage.remove(elem)
+        pygame.draw.circle(screen, (0, 0, 255), character_coordinates, 20)
         pygame.draw.circle(screen, "green", boss_coordinates, 20)
         pygame.display.flip()
         clock.tick(50)
